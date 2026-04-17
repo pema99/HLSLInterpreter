61d12f66831824e4bc4865becb17a27f079efa1f
Pema Malling <pemamalling@gmail.com>
2026-04-17 02:19:16 +0200

    update project structure

--- HLSLInterpreter.Debugger.Web/Program.cs
+++ HLSLInterpreter.Debugger.Web/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using HLSLInterpreter.Web;
+using HLSLInterpreter.Debugger.Web;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
